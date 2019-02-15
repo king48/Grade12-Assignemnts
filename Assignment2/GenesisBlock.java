3ec802e2d52e32e3e34ae37f89ac3b82cf2b1f02
Juan-Diego <juandiego085@gmail.com>
2019-02-15 18:54:50 -0500

    This has nothing yet just method names:

--- Assignment2/GenesisBlock.java
+++ Assignment2/GenesisBlock.java
@@ -1,43 +1,18 @@
 public class GenesisBlock {
-    String privateKey; 
+  String data; 
+  
+  public GenesisBlock (String data) {
+    this.data = data; 
+  }
+  public String getSignature (){
+    String signature = "";
+    GenerateKeys keys = new GenerateKeys (); 
+    String privateKey = keys.getPrivateKey ();
     
-    public void getPrivateKey (){
-      privateKey = ""; 
-      for (int a = 1; a <= 64 ; a ++) {
-        int randomValue = (int)( Math.random () * 16) ; // creates a random value from 0 - 16 
-        if (randomValue <= 9){
-          privateKey += Integer.toString(randomValue); // converts the random value into a string, adds that value to PrivateKey 
-        }
-        else {
-          switch (randomValue){ //finds the value and converts it into a letter 
-            case 10 :
-              privateKey +="A";
-              break; 
-            case 11 : 
-              privateKey +="B"; 
-              break; 
-            case 12 :  
-              privateKey +="C"; 
-              break; 
-            case 13 :  
-              privateKey +="D"; 
-              break; 
-            case 14 :
-              privateKey +="E"; 
-              break;
-            case 15 :
-              privateKey +="F"; 
-              break;
-            case 16 :
-              privateKey +="G"; 
-              break;
-          } // ends switch statment 
-        } // ends else          
-      } // ends for 
-    } // ends method 
+    return signature; 
+  }
+  public void generateHash (){
     
-    public void PublicKey () {
-    
-    }
+  }
   
 }
