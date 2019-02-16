46a3a3d587b9a019868ca7d9045c741b3dc1824b
Juan-Diego <juandiego085@gmail.com>
2019-02-16 12:03:11 -0500

    Is a bit more functional and does give an original output for every input

--- Assignment2/TestHash.java
+++ Assignment2/TestHash.java
@@ -1,18 +1,26 @@
 public class TestHash {
-  public void modulus (){
-   String juan = "my name is juan"; 
-   int g = 7; 
+  public void getHash (String data){
+   int g = 17; 
    int hash =0; 
-   for (int i = 0; i < juan.length(); i ++) 
+   for (int i = 0; i < data.length(); i ++) 
    {
-     hash = (g * hash) + juan.charAt(i); 
+     hash = ((g * hash) + data.charAt(i)); 
     // System.out.println (hash);
    }
-   
+   if (hash < 0) { // if our hash is negative 
+      hash *= -1; 
+    }
     System.out.println ("Final "+hash); 
   }
   public static void main (String args[]){
   TestHash t = new TestHash (); 
-  t.modulus (); 
+  t.getHash ("The name of me is juan"); 
+  t.getHash ("The name of me is juan"); 
+  t.getHash ("The name of me is juan.f"); 
+  t.getHash ("The name of me is juan.123"); 
+  t.getHash ("The people"); 
+  t.getHash ("The name of me i"); 
+      
+  
   }
 }
