8b8ab1f5b74d458394d1f394cc887702e13f9753
Juan-Diego <juandiego085@gmail.com>
2019-02-15 22:00:23 -0500

    Finally made code that actually makes a public key from the private key

--- Assignment2/GenerateKeys.java
+++ Assignment2/GenerateKeys.java
@@ -1,22 +1,17 @@
 public class GenerateKeys{
    private String privateKey = ""; 
-   private String publicKey = "123123123123asdasd"; 
-   public int [] positionOfNumbers = new int [64]; 
-   public int [] positionOfChar = new int [64] ; 
-  
+   private String publicKey = ""; 
+   public boolean [] valueOfPosition = new boolean [64]; 
+   
     public String getPrivateKey (){
-       int amountOfNumbers = 0; 
-       int amountOfChar = 0; 
       for (int a = 0; a < 64 ; a ++) {
         int randomValue = (int)( Math.random () * 15) ; // creates a random value from 0 - 15 
         if (randomValue <= 9){
           privateKey += Integer.toString(randomValue); // converts the random value into a string, adds that value to PrivateKey 
-          positionOfNumbers [amountOfNumbers] = a; // stores the position of where that number is in the privateKey 
-          amountOfNumbers ++; // moves the head of the array up one
+          valueOfPosition [a] = true; // stores the position of where that number is in the privateKey 
         }
         else {
-          positionOfChar [amountOfChar] = a; 
-          amountOfChar ++; 
+          valueOfPosition [a] = false ; 
           switch (randomValue){ //finds the value and converts it into a letter 
             case 10 :
               privateKey +="A";
@@ -39,69 +34,91 @@ public class GenerateKeys{
           } // ends switch statment 
         } // ends else          
       } // ends for 
-      System.out.println ("Your Private Key is" + privateKey); 
+      
       return privateKey; 
     } // ends method 
    
-   
+    private char changeNumber (int position){
+      char tempNumber = privateKey.charAt (position); 
+      switch (tempNumber) {
+        case '0' :
+          tempNumber = '3'; 
+          break;
+        case '1' :
+          tempNumber = '4'; 
+          break; 
+        case '2' :
+          tempNumber = '5'; 
+          break; 
+        case '3' :
+          tempNumber = '6'; 
+          break; 
+        case '4' :
+          tempNumber = '7'; 
+          break;
+        case '5' :
+          tempNumber = '8'; 
+          break;
+        case '6' :
+          tempNumber = '9'; 
+          break; 
+        case '7' :
+          tempNumber = '0'; 
+          break; 
+        case '8' :
+          tempNumber = '1'; 
+          break;
+        case '9' :
+          tempNumber = '2'; 
+          break;
+      }
+      return tempNumber;
+    }
+    private char changeCharacter (int position){
+      char tempChar = privateKey.charAt (position); 
+      switch (tempChar) { 
+        case 'A':
+          tempChar = 'D'; 
+          break; 
+        case 'B' :
+          tempChar = 'E';
+          break;
+        case 'C' :
+          tempChar = 'F'; 
+          break;
+        case 'D':
+          tempChar = 'A'; 
+          break; 
+        case 'E' :
+          tempChar = 'B';
+          break;
+        case 'F' :
+          tempChar = 'C'; 
+          break;                 
+      }
+      return tempChar; 
+    }
+    
     public String getPublicKey () {
+      System.out.println ("  "); 
       for (int a = 0; a < 64 ; a ++) { 
-        if (positionOfNumbers [a] == a) { // if the position of the array is == to the position we are changing then change 
-          char tempNum  = privateKey.charAt (positionOfNumbers [a]); // find the value of the privateKey at this position 
-          if (tempNum >= '0' && tempNum <= '6'){
-            switch (tempNum) {
-              case '0': 
-                tempNum = '3';   
-                break;
-              case '1' : 
-                tempNum = '4'; 
-                break; 
-              case '2':
-                  tempNum = '5'; 
-                  break; 
-              case '3':
-                  tempNum = '6'; 
-                  break; 
-              case '4':
-                tempNum = '7'; 
-                break; 
-              case '5':
-                tempNum = '8'; 
-                break;
-              case '6':
-                tempNum = '9'; 
-                break; 
-              } // end of switch 
-            //System.out.println ("y"+tempNum); 
-            publicKey +=Character.toString(tempNum); // add that number to the privateKey 
-          } else {
-            switch (tempNum) {
-              case '7': 
-                tempNum = '0';   
-                break;
-              case '8' : 
-                tempNum = '1'; 
-                break; 
-              case '9':
-                tempNum = '2'; 
-                break; 
-              } // end of switch 
-          //  System.out.println (tempNum); 
-            publicKey += Character.toString(tempNum); 
-            } // end of else 
-        } // end of second if 
+        if (valueOfPosition [a] == true) {
+          publicKey += Character.toString(changeNumber (a)); 
+        }
         else {
-          
-        } // end of first if statment 
-      }//end of for loop 
-      System.out.println ("Your new public key is " + publicKey); 
+           publicKey += Character.toString(changeCharacter(a)); 
+        }
+      }
       return publicKey; 
     } // end of method 
     
+   /* 
     public static void main (String [] args){
       GenerateKeys k = new GenerateKeys (); 
       k.getPrivateKey(); 
       k.getPublicKey (); 
-
+      System.out.println ("Your Private Key is " + privateKey); 
+      System.out.println ("Your public key is " + publicKey); 
     }
+    */
 }
