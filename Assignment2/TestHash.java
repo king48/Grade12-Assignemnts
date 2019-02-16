3162b9b9692823392573c176f145f1541ae0f6ca
Juan-Diego <juandiego085@gmail.com>
2019-02-16 02:06:45 -0500

    This is...Tbh no progress

--- Assignment2/TestHash.java
+++ Assignment2/TestHash.java
@@ -1,9 +1,15 @@
 public class TestHash {
   public void modulus (){
-    String juan = "my name is juan"; 
-    int length = juan.length (); 
-    int other = 64%length; 
-    System.out.println (length + " " + other); 
+   String juan = "my name is juan"; 
+   int g = 7; 
+   int hash =0; 
+   for (int i = 0; i < juan.length(); i ++) 
+   {
+     hash = (g * hash) + juan.charAt(i); 
+    // System.out.println (hash);
+   }
+   
+    System.out.println ("Final "+hash); 
   }
   public static void main (String args[]){
   TestHash t = new TestHash (); 
