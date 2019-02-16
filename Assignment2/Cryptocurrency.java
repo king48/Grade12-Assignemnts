3d61c963853748a0f140af12d8a0073633400c41
Juan-Diego <juandiego085@gmail.com>
2019-02-16 02:03:49 -0500

    Started creating objects:

--- Assignment2/Cryptocurrency.java
+++ Assignment2/Cryptocurrency.java
@@ -9,6 +9,12 @@ This Program is the main class for running other programms
 public class Cryptocurrency {
     
   public static void main (String [] args){
+    GenerateKeys keys = new GenerateKeys ();
+    GenerateKeys key1 = new GenerateKeys (); 
+    keys.getPrivateKey (); 
+    key1.getPrivateKey (); 
+    GenesisBlock gBlock = new GenesisBlock("10", keys.getPublicKey (), key1.getPublicKey ());
+   
     
   }
 }
