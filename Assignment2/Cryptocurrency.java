405750a435a107626962c52b4eb4559e0c44a6eb
Juan-Diego <juandiego085@gmail.com>
2019-02-16 11:58:48 -0500

    Made changes to variables and added signature

--- Assignment2/Cryptocurrency.java
+++ Assignment2/Cryptocurrency.java
@@ -9,12 +9,11 @@ This Program is the main class for running other programms
 public class Cryptocurrency {
     
   public static void main (String [] args){
-    GenerateKeys keys = new GenerateKeys ();
-    GenerateKeys key1 = new GenerateKeys (); 
-    keys.getPrivateKey (); 
-    key1.getPrivateKey (); 
-    GenesisBlock gBlock = new GenesisBlock("10", keys.getPublicKey (), key1.getPublicKey ());
-   
-    
+    GenerateKeys juan = new GenerateKeys ();
+    GenerateKeys bill = new GenerateKeys (); 
+    juan.getPrivateKey (); 
+    bill.getPrivateKey (); 
+    GenesisBlock gBlock = new GenesisBlock("10$", juan.getPublicKey (), bill.getPublicKey ());
+    gBlock.getSignature (juan.getPrivateKey ());
   }
 }
