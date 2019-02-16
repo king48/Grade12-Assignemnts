032e5763f7172d89d8df1d371473717d2ebb2f03
Juan-Diego <juandiego085@gmail.com>
2019-02-16 16:17:58 -0500

    This is just the main class, only made some comments

--- Assignment2/Cryptocurrency.java
+++ Assignment2/Cryptocurrency.java
@@ -8,7 +8,7 @@ This Program is the main class for running other programms
 
 public class Cryptocurrency {
   
-  static int transactionNumber =1; 
+  static int transactionNumber = 2; 
   
   public static void main (String [] args){
     GenerateKeys juan = new GenerateKeys ();
@@ -19,7 +19,9 @@ public class Cryptocurrency {
     jim.getPrivateKey ();   
     GenerateKeys tan = new GenerateKeys (); 
     tan.getPrivateKey ();
-    GenesisBlock gBlock = new GenesisBlock(transactionNumber,"10$", juan.getPublicKey (), bill.getPublicKey ());
+    GenesisBlock gBlock = new GenesisBlock(transactionNumber,"20$", juan.getPublicKey (), bill.getPublicKey ());
     gBlock.getSignature (juan.getPrivateKey ());
+   // gBlock.proofOfWork (); // isnt working rn 
+    
   }
 }
