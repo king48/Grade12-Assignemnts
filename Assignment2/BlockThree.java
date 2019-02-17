1aa11823ced9e1c1bf69f170ae69e3e70a1a7e89
Juan-Diego <juandiego085@gmail.com>
2019-02-16 22:55:50 -0500

    this is the same as block two.

---
+++ Assignment2/BlockThree.java
@@ -0,0 +1,8 @@
+public class BlockThree extends GenesisBlock {
+ 
+  public BlockThree (){
+  }
+  public BlockThree (int transactionID, String transactionAmount, String sender, String receiver){
+    
+  }
+}
