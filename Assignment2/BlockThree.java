44ffc98673c8ed8e47eebf98ceb9dc71494441c4
Juan-Diego <juandiego085@gmail.com>
2019-02-17 15:26:00 -0500

    This also has bills code ty:

--- Assignment2/BlockThree.java
+++ Assignment2/BlockThree.java
@@ -1,8 +1,16 @@
 public class BlockThree extends GenesisBlock {
- 
-  public BlockThree (){
+  private int transactionsMade;
+
+  public BlockThree(int transactionID, String transactionAmount , String sender, String receiver){
+    super(transactionID,transactionAmount,sender,receiver); //This calls superclass constructor
+    transactionsMade = 0;
   }
-  public BlockThree (int transactionID, String transactionAmount, String sender, String receiver){
-    
+  
+  public void addTransaction(int transactionsMade){
+    this.transactionsMade += transactionsMade;
+  }
+  
+  public int getTransactionsMade(){
+    return transactionsMade;
   }
 }
