4a85a191b583df7c44001544c389e4f233ea1318
Juan-Diego <juandiego085@gmail.com>
2019-02-16 22:55:32 -0500

    This is the subclass to GenesisBlock and is the second, has nothing but a constructor in it

---
+++ Assignment2/BlockTwo.java
@@ -0,0 +1,8 @@
+public class BlockTwo extends GenesisBlock {
+ 
+  public BlockTwo (){
+  }
+  public BlockTwo (int transactionID, String transactionAmount, String sender, String receiver){
+    
+  }
+}
