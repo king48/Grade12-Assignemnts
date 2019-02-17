fe57e4a098ab9e51c759c9a194bd164b4f0e8572
Juan-Diego <juandiego085@gmail.com>
2019-02-17 15:25:41 -0500

    This acc has bills code whooops

--- Assignment2/BlockTwo.java
+++ Assignment2/BlockTwo.java
@@ -1,8 +1,22 @@
 public class BlockTwo extends GenesisBlock {
- 
-  public BlockTwo (){
-  }
-  public BlockTwo (int transactionID, String transactionAmount, String sender, String receiver){
-    
-  }
+
+   private int transactionsMade;
+   private String previousHash;
+   
+   public BlockTwo(int transactionID, String transactionAmount , String sender, String receiver ){
+       super(transactionID,transactionAmount,sender,receiver); //This calls superclass constructor
+       transactionsMade = 0;
+       this.previousHash = previousHash; 
+   }
+  
+   public void addTransaction(int transactionsMade){
+       this.transactionsMade += transactionsMade;
+   }
+  
+   public int getTransactionsMade(){
+       return transactionsMade;
+   }
+
+
+  
 }
