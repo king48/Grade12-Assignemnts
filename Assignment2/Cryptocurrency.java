ae79f30c9c87ba2e601a7d325fd9228f61c86a73
Juan-Diego <juandiego085@gmail.com>
2019-02-18 13:37:13 -0500

    Very small changes have been made

--- Assignment2/Cryptocurrency.java
+++ Assignment2/Cryptocurrency.java
@@ -9,24 +9,49 @@ This Program is the main class for running other programms
 public class Cryptocurrency {
   
   static String [] previousHash = new String [3]; 
+  static int [] transactionNumber = new int [4]; 
+  static double [] transactionAmount = new double [4];  
+  
+  public void firstBlock (String senderKey, String receiverKey){
+    System.out.println ("Juan is sending Bill 20 dollars"); 
+    System.out.println ("Juan is otherwise known as " + senderKey);
+    System.out.println ("And Bill is otherwise known as " + receiverKey);
+    System.out.println (" "); 
+    transactionAmount [0] = 1034.50; 
+    transactionNumber [0] = 1;  
+  }
+  public void secondBlock (String senderKey, String receiverKey){
+    System.out.println ("Jim is sending Josh 5000 dollars"); 
+    System.out.println ("Jim is otherwise known as " + senderKey);
+    System.out.println ("And Josh is otherwise known as " + receiverKey);
+    System.out.println (" "); 
+    transactionAmount [1] = 20.123; 
+    transactionNumber [1] = 2;  
+  }
   
   public static void main (String [] args){
+    Cryptocurrency crypto = new Cryptocurrency (); 
+    
     GenerateKeys juan = new GenerateKeys ();
     GenerateKeys bill = new GenerateKeys (); 
     GenerateKeys jim = new GenerateKeys (); 
     GenerateKeys josh = new GenerateKeys (); 
     GenerateKeys jen = new GenerateKeys (); 
     GenerateKeys jess = new GenerateKeys ();
-   
-    GenesisBlock genBlock = new GenesisBlock(1,"20$", juan.getPublicKey (), bill.getPublicKey ());
-    previousHash [0]= genBlock.generateBlock (juan.getPrivateKey ());
+    GenerateKeys lenn = new GenerateKeys (); 
+    GenerateKeys glenn = new GenerateKeys ();
+    
+    crypto.firstBlock (juan.getPublicKey (), bill.getPublicKey ());
+    GenesisBlock genBlock = new GenesisBlock(transactionNumber [0], transactionAmount [0], juan.getPublicKey (), bill.getPublicKey ());
+    previousHash [0] = genBlock.generateBlock (juan.getPrivateKey ());
     System.out.println (" ");
-    // BlockTwo secondBlock = new BlockTwo(2,"5000$", jim.getPublicKey (), josh.getPublicKey (), previousHash [0]);
-    // previousHash [1]= secondBlock.generateBlock (juan.getPrivateKey ());
-    // System.out.println (" ");
-    //  BlockThree thirdBlock = new BlockThree(3,"1250$",  jen.getPublicKey (), jess.getPublicKey (), previousHash [1]);
-    // previousHash [2]= thirdBlock.generateBlock (juan.getPrivateKey ());
-    //  System.out.println (" ");
+    crypto.secondBlock (jim.getPublicKey (), josh.getPublicKey ());
+    BlockTwo secondBlock = new BlockTwo(transactionNumber [1], transactionAmount [0], jim.getPublicKey (), josh.getPublicKey (), previousHash [0]);
+    previousHash [1] = secondBlock.generateBlock (jim.getPrivateKey ());
+    System.out.println (" ");
+  //  BlockThree thirdBlock = new BlockThree(3,"1250$",  jen.getPublicKey (), jess.getPublicKey (), previousHash [1]);
+   // previousHash [2]= thirdBlock.generateBlock (juan.getPrivateKey ());
+  //  System.out.println (" ");
     
   }
 }
